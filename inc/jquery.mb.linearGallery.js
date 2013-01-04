78c692c957a04ba81e590948c04f8900de4f0473
pupunzi <matteo@open-lab.com>
2013-01-04 17:43:22 +0100

    various

--- inc/jquery.mb.linearGallery.js
+++ inc/jquery.mb.linearGallery.js
@@ -1,21 +1,21 @@
 /*
  * ******************************************************************************
  *  jquery.mb.components
+ *  file: jquery.mb.linearGallery.js
  *
- *  Copyright (c) 2001-2013. Matteo Bicocchi (Pupunzi); Open lab srl, Firenze - Italy
+ *  Copyright (c) 2001-2013. Matteo Bicocchi (Pupunzi);
+ *  Open lab srl, Firenze - Italy
  *  email: matteo@open-lab.com
- *  site: http://pupunzi.com
+ *  site: 	http://pupunzi.com
+ *  blog:	http://pupunzi.open-lab.com
+ * 	http://open-lab.com
  *
  *  Licences: MIT, GPL
  *  http://www.opensource.org/licenses/mit-license.php
  *  http://www.gnu.org/licenses/gpl.html
+ *
+ *  last modified: 04/01/13 17.13
  *  *****************************************************************************
- */
-
-/*
- * jQuery.mb.components: jquery.mb.linearGallery
- * version: 1.0
- * © 2001 - 2011 Matteo Bicocchi (pupunzi), Open Lab
  */
 
 /*******************************************************************************
