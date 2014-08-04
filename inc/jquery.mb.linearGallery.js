6b4b463d686a4312b5bca3097baf43c5b9d67b0f
Matteo Bicocchi <matteo@open-lab.com>
2014-08-04 16:00:55 +0200

    fixed jquery-animate-rotate compatibility issue

--- inc/jquery.mb.linearGallery.js
+++ inc/jquery.mb.linearGallery.js
@@ -42,7 +42,138 @@ if (!jQuery.browser) {
  inclusion jquery-animate-css-rotate-scale
  ******************************************************************************/
 
-(function(b){var d=null,i=b.fn.css;b.fn.css=function(a,c){d===null&&(d=typeof b.cssProps!="undefined"?b.cssProps:typeof b.props!="undefined"?b.props:{});if(typeof d.transform=="undefined"&&(a=="transform"||typeof a=="object"&&typeof a.transform!="undefined")){var e=d,f;a:{f=this.get(0);for(var h=["transform","WebkitTransform","msTransform","MozTransform","OTransform"],g;g=h.shift();)if(typeof f.style[g]!="undefined"){f=g;break a}f="transform"}e.transform=f}if(d.transform!="transform")if(a=="transform"){if(a= d.transform,typeof c=="undefined"&&jQuery.style)return jQuery.style(this.get(0),a)}else if(typeof a=="object"&&typeof a.transform!="undefined")a[d.transform]=a.transform,delete a.transform;return i.apply(this,arguments)};var e="deg";b.fn.rotate=function(a){var c=b(this).css("transform")||"none";if(typeof a=="undefined"){if(c&&(a=c.match(/rotate\(([^)]+)\)/))&&a[1])return a[1];return 0}if(a=a.toString().match(/^(-?\d+(\.\d+)?)(.+)?$/))a[3]&&(e=a[3]),b(this).css("transform",c.replace(/none|rotate\([^)]*\)/, "")+"rotate("+a[1]+e+")");return this};b.fn.scale=function(a){var c=b(this).css("transform");if(typeof a=="undefined"){if(c&&(a=c.match(/scale\(([^)]+)\)/))&&a[1])return a[1];return 1}b(this).css("transform",c.replace(/none|scale\([^)]*\)/,"")+"scale("+a+")");return this};var h=b.fx.prototype.cur;b.fx.prototype.cur=function(){if(this.prop=="rotate")return parseFloat(b(this.elem).rotate());else if(this.prop=="scale")return parseFloat(b(this.elem).scale());return h.apply(this,arguments)};b.fx.step.rotate= function(a){b(a.elem).rotate(a.now+e)};b.fx.step.scale=function(a){b(a.elem).scale(a.now)};var j=b.fn.animate;b.fn.animate=function(a){if(typeof a.rotate!="undefined"){var b=a.rotate.toString().match(/^(([+-]=)?(-?\d+(\.\d+)?))(.+)?$/);b&&b[5]&&(e=b[5]);a.rotate=b[1]}return j.apply(this,arguments)}})(jQuery);
+//(function(b){var d=null,i=b.fn.css;b.fn.css=function(a,c){d===null&&(d=typeof b.cssProps!="undefined"?b.cssProps:typeof b.props!="undefined"?b.props:{});if(typeof d.transform=="undefined"&&(a=="transform"||typeof a=="object"&&typeof a.transform!="undefined")){var e=d,f;a:{f=this.get(0);for(var h=["transform","WebkitTransform","msTransform","MozTransform","OTransform"],g;g=h.shift();)if(typeof f.style[g]!="undefined"){f=g;break a}f="transform"}e.transform=f}if(d.transform!="transform")if(a=="transform"){if(a= d.transform,typeof c=="undefined"&&jQuery.style)return jQuery.style(this.get(0),a)}else if(typeof a=="object"&&typeof a.transform!="undefined")a[d.transform]=a.transform,delete a.transform;return i.apply(this,arguments)};var e="deg";b.fn.rotate=function(a){var c=b(this).css("transform")||"none";if(typeof a=="undefined"){if(c&&(a=c.match(/rotate\(([^)]+)\)/))&&a[1])return a[1];return 0}if(a=a.toString().match(/^(-?\d+(\.\d+)?)(.+)?$/))a[3]&&(e=a[3]),b(this).css("transform",c.replace(/none|rotate\([^)]*\)/, "")+"rotate("+a[1]+e+")");return this};b.fn.scale=function(a){var c=b(this).css("transform");if(typeof a=="undefined"){if(c&&(a=c.match(/scale\(([^)]+)\)/))&&a[1])return a[1];return 1}b(this).css("transform",c.replace(/none|scale\([^)]*\)/,"")+"scale("+a+")");return this};var h=b.fx.prototype.cur;b.fx.prototype.cur=function(){if(this.prop=="rotate")return parseFloat(b(this.elem).rotate());else if(this.prop=="scale")return parseFloat(b(this.elem).scale());return h.apply(this,arguments)};b.fx.step.rotate= function(a){b(a.elem).rotate(a.now+e)};b.fx.step.scale=function(a){b(a.elem).scale(a.now)};var j=b.fn.animate;b.fn.animate=function(a){if(typeof a.rotate!="undefined"){var b=a.rotate.toString().match(/^(([+-]=)?(-?\d+(\.\d+)?))(.+)?$/);b&&b[5]&&(e=b[5]);a.rotate=b[1]}return j.apply(this,arguments)}})(jQuery);
+
+
+/*!
+ /**
+ * Monkey patch jQuery 1.3.1+ to add support for setting or animating CSS
+ * scale and rotation independently.
+ * https://github.com/zachstronaut/jquery-animate-css-rotate-scale
+ * Released under dual MIT/GPL license just like jQuery.
+ * 2009-2012 Zachary Johnson www.zachstronaut.com
+ */
+(function ($) {
+	// Updated 2010.11.06
+	// Updated 2012.10.13 - Firefox 16 transform style returns a matrix rather than a string of transform functions.  This broke the features of this jQuery patch in Firefox 16.  It should be possible to parse the matrix for both scale and rotate (especially when scale is the same for both the X and Y axis), however the matrix does have disadvantages such as using its own units and also 45deg being indistinguishable from 45+360deg.  To get around these issues, this patch tracks internally the scale, rotation, and rotation units for any elements that are .scale()'ed, .rotate()'ed, or animated.  The major consequences of this are that 1. the scaled/rotated element will blow away any other transform rules applied to the same element (such as skew or translate), and 2. the scaled/rotated element is unaware of any preset scale or rotation initally set by page CSS rules.  You will have to explicitly set the starting scale/rotation value.
+
+	function initData($el) {
+		var _ARS_data = $el.data('_ARS_data');
+		if (!_ARS_data) {
+			_ARS_data = {
+				rotateUnits: 'deg',
+				scale: 1,
+				rotate: 0
+			};
+
+			$el.data('_ARS_data', _ARS_data);
+		}
+
+		return _ARS_data;
+	}
+
+	function setTransform($el, data) {
+		$el.css('transform', 'rotate(' + data.rotate + data.rotateUnits + ') scale(' + data.scale + ',' + data.scale + ')');
+	}
+
+	$.fn.rotate = function (val) {
+		var $self = $(this), m, data = initData($self);
+
+		if (typeof val == 'undefined') {
+			return data.rotate + data.rotateUnits;
+		}
+
+		m = val.toString().match(/^(-?\d+(\.\d+)?)(.+)?$/);
+		if (m) {
+			if (m[3]) {
+				data.rotateUnits = m[3];
+			}
+
+			data.rotate = m[1];
+
+			setTransform($self, data);
+		}
+
+		return this;
+	};
+
+	// Note that scale is unitless.
+	$.fn.scale = function (val) {
+		var $self = $(this), data = initData($self);
+
+		if (typeof val == 'undefined') {
+			return data.scale;
+		}
+
+		data.scale = val;
+
+		setTransform($self, data);
+
+		return this;
+	};
+
+	// fx.cur() must be monkey patched because otherwise it would always
+	// return 0 for current rotate and scale values
+	var curProxied = $.fx.prototype.cur;
+	$.fx.prototype.cur = function () {
+		if (this.prop == 'rotate') {
+			return parseFloat($(this.elem).rotate());
+
+		} else if (this.prop == 'scale') {
+			return parseFloat($(this.elem).scale());
+		}
+
+		return curProxied.apply(this, arguments);
+	};
+
+	$.fx.step.rotate = function (fx) {
+		var data = initData($(fx.elem));
+		$(fx.elem).rotate(fx.now + data.rotateUnits);
+	};
+
+	$.fx.step.scale = function (fx) {
+		$(fx.elem).scale(fx.now);
+	};
+
+	/*
+
+	 Starting on line 3905 of jquery-1.3.2.js we have this code:
+
+	 // We need to compute starting value
+	 if ( unit != "px" ) {
+	 self.style[ name ] = (end || 1) + unit;
+	 start = ((end || 1) / e.cur(true)) * start;
+	 self.style[ name ] = start + unit;
+	 }
+
+	 This creates a problem where we cannot give units to our custom animation
+	 because if we do then this code will execute and because self.style[name]
+	 does not exist where name is our custom animation's name then e.cur(true)
+	 will likely return zero and create a divide by zero bug which will set
+	 start to NaN.
+
+	 The following monkey patch for animate() gets around this by storing the
+	 units used in the rotation definition and then stripping the units off.
+
+	 */
+
+	var animateProxied = $.fn.animate;
+	$.fn.animate = function (prop) {
+		if (typeof prop['rotate'] != 'undefined') {
+			var $self, data, m = prop['rotate'].toString().match(/^(([+-]=)?(-?\d+(\.\d+)?))(.+)?$/);
+			if (m && m[5]) {
+				$self = $(this);
+				data = initData($self);
+				data.rotateUnits = m[5];
+			}
+
+			prop['rotate'] = m[1];
+		}
+
+		return animateProxied.apply(this, arguments);
+	};
+})(jQuery);
+
 
 /*******************************************************************************
  end inclusion
